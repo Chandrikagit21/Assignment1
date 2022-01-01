81f51bbe6073be5d9eb0be44101852f9ee435365
Chandrikagit21 <95068840+Chandrikagit21@users.noreply.github.com>
2022-01-01 14:58:27 +0530

    initial commit

---
+++ javascript/js1.js
@@ -0,0 +1,28 @@
+//Q1  
+
+let fname="John"
+let admin="jiii"
+admin=alert(fname+admin)
+console.log(fname)
+//Q2
+const yellow="#f3ca28"
+console.log(yellow)
+//Q3
+
+let string="I am Pretty"
+let num=11
+console.log(typeof (num))
+console.log(typeof (string))
+// Q4
+
+console.log(5>4)
+console.log("apple">"pineapple")
+console.log("2">"12")
+console.log(undefined==null)
+console.log(undefined===null)
+    console.log(null=="\n0\n")
+    console.log(null==="\n0\n")
+//Q5
+
+let pname=prompt("Enter Your Name")
+console.log(pname)
